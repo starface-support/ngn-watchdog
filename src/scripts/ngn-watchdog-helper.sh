8073d8f3b3cb31d15eb841ea00168ffdb3635b8e
Niklas Janz (STARFACE Support) <sf-janz@users.noreply.github.com>
2019-08-30 16:38:00 +0200

    added sh skeleton

---
+++ src/scripts/ngn-watchdog-helper.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# TBD
+
+# for p in "$@"
+# do
+#   case "$p" in
+#     "Nichts")
+#       return 0 ;;
+#     "Asterisk neustarten (Hart)")
+#       service asterisk restart ;;
+#     "Asterisk neustarten (Gesprächende abwarten)")
+#       /usr/sbin/asterisk -x "core restart gracefully";;
+#     "Diensteneustart")
+#       ;;
+#     "SIP Reload")
+#       ;;
+#   esac
+# done
+
